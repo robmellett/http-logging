35efe60de63bee5302dba7176698c9ccc5a8b31f
Rob Mellett <dev@robmellett.com>
2023-08-15 22:11:15 +0700

    scaffolding

--- src/HttpLogging.php
+++ src/HttpLogging.php
@@ -2,6 +2,30 @@
 
 namespace RobMellett\HttpLogging;
 
+use Psr\Http\Message\RequestInterface;
+
 class HttpLogging
 {
+    // public function __invoke(callable $handler)
+    // {
+    //     return function (RequestInterface $request, array $options) use ($handler) {
+    //         logger("This is from the request middleware class");
+    //
+    //         return $handler($request, $options);
+    //     };
+    // }
+
+    public function __invoke(callable $handler)
+    {
+        return function (RequestInterface $request, array $options)  use ($handler) {
+            logger("This is from the request middleware class");
+
+            return $handler($request, $options)->then(
+                function ($response) {
+                    logger("This is from the response middleware class");
+                    return $response;
+                }
+            );
+        };
+    }
 }
--- tests/Feature/HttpLoggingTest.php
+++ tests/Feature/HttpLoggingTest.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use RobMellett\HttpLogging\HttpLogging;
 use RobMellett\HttpLogging\Tests\TestCase;
 
 class HttpLoggingTest extends TestCase
@@ -32,6 +33,17 @@ class HttpLoggingTest extends TestCase
                 return $response;
             }
         )
+            ->get('https://jsonplaceholder.typicode.com/posts');
+
+        $this->assertTrue($response->ok());
+    }
+
+    /** @test */
+    public function can_fetch_requests_with_middlware_with_class()
+    {
+        Http::buildHandlerStack()->push(new HttpLogging());
+
+        $response = Http::withMiddleware(new HttpLogging())
             ->get('https://jsonplaceholder.typicode.com/posts');
 
         $this->assertTrue($response->ok());
