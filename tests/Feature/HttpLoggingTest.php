18ccec383e03d75652385ad817813a4f7edcecd9
Rob Mellett <dev@robmellett.com>
2023-08-15 21:45:19 +0700

    wip

---
+++ tests/Feature/HttpLoggingTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RobMellett\HttpLogging\Tests\Feature;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use RobMellett\HttpLogging\Tests\TestCase;
+
+class HttpLoggingTest extends TestCase
+{
+    /** @test */
+    public function can_fetch_requests_without_middlware()
+    {
+        $response = Http::get('https://jsonplaceholder.typicode.com/posts');
+
+        $this->assertTrue($response->ok());
+    }
+
+    /** @test */
+    public function can_fetch_requests_with_middlware()
+    {
+        $response = Http::withRequestMiddleware(
+            function (RequestInterface $request) {
+                logger("This is from the request middleware");
+                return $request;
+            }
+        )->withResponseMiddleware(
+            function (ResponseInterface $response) {
+                logger("This is from the response middleware");
+                return $response;
+            }
+        )
+            ->get('https://jsonplaceholder.typicode.com/posts');
+
+        $this->assertTrue($response->ok());
+    }
+}
