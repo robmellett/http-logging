03889b7d71a51a65e8c8d8880cf6caa98f146d2e
Rob Mellett <dev@robmellett.com>
2023-08-15 23:28:14 +0700

    Tidy up methods

--- src/HttpLogging.php
+++ src/HttpLogging.php
@@ -3,6 +3,7 @@
 namespace RobMellett\HttpLogging;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -10,33 +11,43 @@ class HttpLogging
 {
     public function __invoke(callable $handler)
     {
-        $uuid = uniqid();
+        $uuid = app(Str::class)->uuid();
 
         return function (RequestInterface $request, array $options) use ($handler, $uuid) {
-            Log::debug("Request {$uuid}", [
-                'request_id' => $uuid,
-                'method' => $request->getMethod(),
-                'uri' => [
-                    'scheme' => $request->getUri()->getScheme(),
-                    'host' => $request->getUri()->getHost(),
-                    'path' => $request->getUri()->getPath(),
-                    'query' => $request->getUri()->getQuery(),
-                ],
-                'headers' => $request->getHeaders(),
-            ]);
-
+            $this->logRequest($uuid, $request);
+            
             return $handler($request, $options)->then(
                 function (ResponseInterface $response) use ($uuid) {
-                    Log::debug("Response {$uuid}", [
-                        'response_id' => $uuid,
-                        'status_code' => $response->getStatusCode(),
-                        'headers' => $response->getHeaders(),
-                        'body' => json_decode($response->getBody(), true),
-                    ]);
+                    $this->logResponse($uuid, $response);
 
                     return $response;
                 }
             );
         };
+    }
+
+    private function logRequest(string $uuid, RequestInterface $request): void
+    {
+        Log::debug("Request {$uuid}", [
+            'request_id' => $uuid,
+            'method' => $request->getMethod(),
+            'uri' => [
+                'scheme' => $request->getUri()->getScheme(),
+                'host' => $request->getUri()->getHost(),
+                'path' => $request->getUri()->getPath(),
+                'query' => $request->getUri()->getQuery(),
+            ],
+            'headers' => $request->getHeaders(),
+        ]);
+    }
+
+    private function logResponse(string $uuid, ResponseInterface $response): void
+    {
+        Log::debug("Response {$uuid}", [
+            'response_id' => $uuid,
+            'status_code' => $response->getStatusCode(),
+            'headers' => $response->getHeaders(),
+            'body' => json_decode($response->getBody(), true),
+        ]);
     }
 }
