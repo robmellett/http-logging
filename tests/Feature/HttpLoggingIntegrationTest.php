f06b7bdc8f9e3e7d6a1c3ade372bb9c8d9c3cc10
Rob Mellett <dev@robmellett.com>
2024-05-31 18:41:17 +0700

    Tidy up test

--- tests/Feature/HttpLoggingIntegrationTest.php
+++ tests/Feature/HttpLoggingIntegrationTest.php
@@ -13,37 +13,6 @@ class HttpLoggingIntegrationTest extends TestCase
     /** @test */
     public function can_write_to_logs_with_secure_json_formatter()
     {
-        if (InstalledVersions::getVersion('monolog/monolog') < '2.0.0') {
-            $this->markTestSkipped('This test is only for Monolog > 2.0.0+');
-        }
-
-        $spy = $this->spy(SecureJsonFormatter::class);
-
-        config()->set('logging.channels.http_logs', [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-
-            'formatter' => SecureJsonFormatter::class,
-        ]);
-
-        Http::withMiddleware(new HttpLogging([
-            'channel' => 'http_logs',
-        ]))
-            ->withToken('Ym9zY236Ym9zY28=')
-            ->asJson()
-            ->get('https://jsonplaceholder.typicode.com/posts?userId=1');
-
-        $spy->shouldHaveReceived('format')->twice();
-    }
-
-    /** @test */
-    public function can_write_to_logs_with_legacy_secure_json_formatter()
-    {
-        if (InstalledVersions::getVersion('monolog/monolog') > '2.0.0') {
-            $this->markTestSkipped('This test is only for Monolog < 2.0.0+');
-        }
-
         $spy = $this->spy(SecureJsonFormatter::class);
 
         config()->set('logging.channels.http_logs', [
